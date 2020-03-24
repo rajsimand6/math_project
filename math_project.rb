bf8604a537b76ed2074ac2f47c760f29e05c65d4
rajsimand6 <simandalahirajesh681@gmail.com>
2020-03-24 15:59:37 +0200

    add matrix module and zeros initialize function

--- math_project.rb
+++ math_project.rb
@@ -1,1 +1,24 @@
 #!/usr/bin/env ruby
+
+module User_matrix
+	def User_matrix.zeros(r, c)
+		# Make sure r/c are positive integers
+		cond1 = r.instance_of?(Integer)
+		cond2 = c.instance_of?(Integer)
+		cond3 = (r > 0)
+		cond4 = (c > 0)
+		
+		if (not cond1) or (not cond2) or (not cond3) or (not cond4)
+			puts "Error: arguments must be positive integers"
+			return
+		end
+		
+		m = Array.new(r){Array.new(c, 0.0)}
+		return m
+	end
+
+
+
+
+
+end
