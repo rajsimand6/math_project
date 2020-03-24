a102fa15e22c1410acdab6ea15f00b7bea8765b3
rajsimand6 <simandalahirajesh681@gmail.com>
2020-03-24 17:32:24 +0200

    add ones initialize function to matrix module

--- math_project.rb
+++ math_project.rb
@@ -17,7 +17,23 @@ module User_matrix
 		return m
 	end
 
-
+	def User_matrix.ones(r, c)
+		# Make sure r/c are positive integers
+		cond1 = r.instance_of?(Integer)
+		cond2 = c.instance_of?(Integer)
+		cond3 = (r > 0)
+		cond4 = (c > 0)
+		
+		if (not cond1) or (not cond2) or (not cond3) or (not cond4)
+			puts "Error: arguments must be positive integers"
+			return
+		end
+		
+		m = Array.new(r){Array.new(c, 1.0)}
+		return m
+	end
+	
+	
 
 
 
