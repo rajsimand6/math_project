b6b26bd9e6a48b0edb3e153bb08b144c721aae61
rajsimand6 <simandalahirajesh681@gmail.com>
2020-03-24 21:34:01 +0200

    add identity initialize function to matrix module

--- math_project.rb
+++ math_project.rb
@@ -33,7 +33,25 @@ module User_matrix
 		return m
 	end
 	
+	def User_matrix.ident(r)
+		# Make sure r is positive integer
+		cond1 = r.instance_of?(Integer)
+		cond2 = (r > 0)
+		
+		if (not cond1) or (not cond2)
+			puts "Error: argument must be positive integer"
+			return
+		end
+		
+		m = User_matrix.zeros(r,r)
+		
+		for i in 0...r
+			m[i][i] = 1.0
+		end
+		
+		return m
 	
+	end
 
 
 
